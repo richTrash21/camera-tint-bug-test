e6f359aaf2d4fddbba2fd064bf612f3779b27c19
richTrash21 <superboss865@gmail.com>
2024-07-15 20:35:27 +0400

    demo

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,18 +1,44 @@
 package;
 
-import flixel.text.FlxText;
+import flixel.FlxG;
+import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
+import flixel.util.FlxTimer;
 
 class PlayState extends FlxState
 {
-	override public function create()
+	static final colors:Array<FlxColor> = [
+		FlxColor.WHITE, FlxColor.GRAY, FlxColor.BLACK, FlxColor.GREEN, FlxColor.LIME, FlxColor.YELLOW, FlxColor.ORANGE, FlxColor.RED, FlxColor.PURPLE,
+		FlxColor.BLUE, FlxColor.BROWN, FlxColor.PINK, FlxColor.MAGENTA, FlxColor.CYAN
+	];
+
+	var curColor:Int = 0;
+
+	override function create():Void
 	{
 		super.create();
-		add(new FlxText("Hello World", 32).screenCenter());
+
+		add(new FlxSprite().makeGraphic(640, 480, FlxColor.WHITE));
+
+		cycleColors();
 	}
 
-	override public function update(elapsed:Float)
+	override function update(elapsed:Float)
 	{
 		super.update(elapsed);
+		// using private __colorTransform to check color transform directly
+		@:privateAccess
+		final transform = (FlxG.renderBlit ? FlxG.camera._flashBitmap.transform.__colorTransform : FlxG.camera.canvas.transform.__colorTransform);
+		final cameraColor = FlxG.camera.color;
+		final cameraTransformColor = FlxColor.fromRGBFloat(transform.redMultiplier, transform.greenMultiplier, transform.blueMultiplier);
+		trace(cameraColor.toHexString(), cameraTransformColor.toHexString(), cameraColor == cameraTransformColor);
+	}
+
+	function cycleColors(?_)
+	{
+		FlxG.camera.color = colors[curColor];
+		curColor = (++curColor % colors.length);
+		new FlxTimer().start(1.0, cycleColors);
 	}
 }
